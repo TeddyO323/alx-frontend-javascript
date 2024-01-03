37e58fbaab76aab9eb9ee3a84321e2337a4d77d4
teddyomondi <theoriginalsomosh@gmail.com>
2024-01-03 18:50:48 +0300

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,10 @@
-export default function handleResponseFromAPI(promise) {
-    promise
-      .then(() => {
-        console.log('Got a response from the API');
-        return {
-          status: 200,
-          body: 'success',
-        };
-      })
-      .catch(() => {
-        return new Error();
-      });
+function handleResponseFromAPI(promise) {
+    const body = { status: 200, body: 'success' };
+  
+    return promise
+      .then(() => body)
+      .catch((error) => error)
+      .finally(() => console.log('Got a response from the API'));
   }
+  export default handleResponseFromAPI;
   
