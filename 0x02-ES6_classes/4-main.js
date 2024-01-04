3148cfe05c4f88709b12f7af457e8d894421f401
teddyomondi <theoriginalsomosh@gmail.com>
2024-01-04 18:46:25 +0300

    p

--- 0x02-ES6_classes/4-main.js
+++ 0x02-ES6_classes/4-main.js
@@ -1,12 +1,6 @@
 import Pricing from './4-pricing';
 import Currency from './3-currency';
 
-const euro = new Currency('EUR', 'Euro');
-const p = new Pricing(100, euro);
-
+const p = new Pricing(100, new Currency("EUR", "Euro"))
 console.log(p);
-console.log(p.displayFullPrice());  // Output: 100 Euro (EUR)
-
-const conversionRate = 1.2;
-const convertedPrice = Pricing.convertPrice(p.amount, conversionRate);
-console.log(`Converted Price: ${convertedPrice}`);
+console.log(p.displayFullPrice());
--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,6 +1,6 @@
 import Currency from './3-currency';
 
-class Pricing {
+export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
@@ -10,25 +10,23 @@ class Pricing {
     return this._amount;
   }
 
-  set amount(value) {
-    this._amount = value;
-  }
-
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    this._currency = value;
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this._amount} ${new Currency(this._currency.code, this._currency.name).displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
-
-export default Pricing;
