b0d47bfbbd1637643c383f8e3bbd05ce4585af15
teddyomondi <theoriginalsomosh@gmail.com>
2024-01-04 18:18:43 +0300

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,40 +1,39 @@
 class HolbertonCourse {
-    constructor(name, length, students) {
-      this._name = name;
-      this._length = length;
-      this._students = students;
-    }
-  
-    // Getter and Setter for 'name'
-    get name() {
-      return this._name;
-    }
-  
-    set name(value) {
-      // Validate the type of 'value' if needed
-      this._name = value;
-    }
-  
-    // Getter and Setter for 'length'
-    get length() {
-      return this._length;
-    }
-  
-    set length(value) {
-      // Validate the type of 'value' if needed
-      this._length = value;
-    }
-  
-    // Getter and Setter for 'students'
-    get students() {
-      return this._students;
-    }
-  
-    set students(value) {
-      // Validate the type of 'value' if needed
-      this._students = value;
-    }
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
-  
-  export default HolbertonCourse;
-  
+
+  // Getter and Setter for 'name'
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    // Validate the type of 'value' if needed
+    this._name = value;
+  }
+
+  // Getter and Setter for 'length'
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    // Validate the type of 'value' if needed
+    this._length = value;
+  }
+
+  // Getter and Setter for 'students'
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    // Validate the type of 'value' if needed
+    this._students = value;
+  }
+}
+
+export default HolbertonCourse;
