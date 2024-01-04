e46413bedda460056671c0066c54c2a37106ea48
teddyomondi <theoriginalsomosh@gmail.com>
2024-01-04 18:27:35 +0300

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,38 +1,50 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
+    this._name = this.validateString(name, 'Name');
+    this._length = this.validateNumber(length, 'Length');
+    this._students = students;
+  }
+
+  validateString(value, propertyName) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${propertyName} must be a string`);
+    }
+    return value;
+  }
+
+  validateNumber(value, propertyName) {
+    if (typeof value !== 'number') {
+      throw new TypeError(`${propertyName} must be a number`);
+    }
+    return value;
   }
 
   // Getter and Setter for 'name'
   get name() {
-    return this.name;
+    return this._name;
   }
 
   set name(value) {
-    // Validate the type of 'value' if needed
-    this.name = value;
+    this._name = this.validateString(value, 'Name');
   }
 
   // Getter and Setter for 'length'
   get length() {
-    return this.length;
+    return this._length;
   }
 
   set length(value) {
-    // Validate the type of 'value' if needed
-    this.length = value;
+    this._length = this.validateNumber(value, 'Length');
   }
 
   // Getter and Setter for 'students'
   get students() {
-    return this.students;
+    return this._students;
   }
 
   set students(value) {
     // Validate the type of 'value' if needed
-    this.students = value;
+    this._students = value;
   }
 }
 
