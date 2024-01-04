414457b7d878f95cb01e19e3131a48d80fd22a35
teddyomondi <theoriginalsomosh@gmail.com>
2024-01-04 12:26:45 +0300

    task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,17 @@
+export default class ClassRoom {
+    /**
+     * @param {number} maxStudentsSize - The maximum number of students allowed in the classroom.
+     */
+    constructor(maxStudentsSize) {
+      this._maxStudentsSize = maxStudentsSize;
+    }
+  
+    /**
+     * Get the maximum number of students allowed in the classroom.
+     * @returns {number} - The maximum number of students.
+     */
+    get maxStudentsSize() {
+      return this._maxStudentsSize;
+    }
+  }
+  
