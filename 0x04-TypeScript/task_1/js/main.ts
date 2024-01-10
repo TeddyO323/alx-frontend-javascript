400b2e059b3019c2106187c87fb1b8835f7ac680
teddyomondi <theoriginalsomosh@gmail.com>
2024-01-10 21:57:22 +0300

    task

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,21 @@
+// Create an interface named Teacher
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any; // Allow any additional attribute
+  }
+  
+  // Example usage
+  const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  
